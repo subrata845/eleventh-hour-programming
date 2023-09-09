3f0665bdbb702fb90535d2fc40328b1d808c78c8
Subrata Saha <saha.cse.pstu@gmail.com>
2023-09-09 12:59:37 +0600

    third

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -16,28 +16,34 @@ const shop = () => {
     return (
 <div className='product-container'>
  
-        <div className="row">
-          <div className="col-md-4 card card">
+        <div className="row ">
+          <div className="col-md-4 card px-md-5 card">
                   <img src={java_logo} className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">Java: Building Blocks of Tech</h5>
                  <p className="card-text">Master the art of object-oriented programming with our Java course.</p>
+                 <h3 class="price">$300</h3>
+                <button class="buy-now-button">Buy Now</button>
              </div>
            </div>
        
-        <div className="col-md-4 card">
+        <div className="col-md-4 px-md-5 card">
       <img src={js_logo} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">JavaScript: Animate the Web </h5>
         <p className="card-text">Bring your websites to life with our hands-on JavaScript course.</p>
+        <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
       </div>
     </div>
     
-        <div className="col-md-4 card">
+        <div className="col-md-4 px-md-5 card">
       <img src={c_sharp} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">C#: Power in Your Hands</h5>
         <p className="card-text">Develop powerful applications with our comprehensive C# course.</p>
+        <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
       </div>
       </div>
       
@@ -45,11 +51,13 @@ const shop = () => {
 
 
   <div className="row">
-          <div className="col-md-4 card">
+          <div className="col-md-4 px-md-5 card">
                   <img src={swift_logo} className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">Swift: Speed Up App Development</h5>
                  <p className="card-text">Fast-track your iOS app development with our Swift course.</p>
+                 <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
              </div>
            </div>
        
@@ -58,6 +66,8 @@ const shop = () => {
       <div className="card-body">
         <h5 className="card-title">MySQL: Manage Your Data </h5>
         <p className="card-text">Learn to organize and manipulate data with our MySQL course.</p>
+        <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
       </div>
     </div>
     
@@ -66,6 +76,8 @@ const shop = () => {
       <div className="card-body">
         <h5 className="card-title">Kotlin: Simplify Android Development </h5>
         <p className="card-text">Make Android development a breeze with our Kotlin course.</p>
+        <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
       </div>
       </div>
       
@@ -78,6 +90,8 @@ const shop = () => {
       <div className="card-body">
         <h5 className="card-title">Ruby: Jewel of the Web </h5>
         <p className="card-text">Create beautiful web applications with our engaging Ruby course.</p>
+        <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
       </div>
        </div> 
        <div className="col-md-4 card">
@@ -85,6 +99,8 @@ const shop = () => {
       <div className="card-body">
         <h5 className="card-title">Python: Unleash the Power of Data</h5>
         <p className="card-text">Dive into data science with our interactive Python course.</p>
+        <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
       </div>
        </div>
 
@@ -93,6 +109,8 @@ const shop = () => {
               <div className="card-body">
                 <h5 className="card-title">Swift: Speed Up App Development</h5>
                  <p className="card-text">Fast-track your iOS app development with our Swift course.</p>
+                 <h3 class="price">$300</h3>
+        <button class="buy-now-button">Buy Now</button>
              </div>
            </div>
        </div>
